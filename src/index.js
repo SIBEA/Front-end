35323fa815fe1c8bc4e3b4cedf93a4c7306479a2
dnchaparro <dnchaparro@ms-dnchaparro>
2023-02-23 15:41:16 -0500

    Se agrega la pantalla de detalle de la investidación (mockup 4).

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Content from './content/content';
 import './index.css';
 import Search from './main search/search';
 import Profile from './profile/profile';
@@ -8,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Profile />
+    <Content />
   </React.StrictMode>
 );
 
