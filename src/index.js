c1810a0db6e80f6f9f76b2fd704a405c4b20d4dc
Diego Sanchez <diegosl2607@gmail.com>
2023-02-27 21:37:14 -0500

    Pantalla de grupos de investigación terminada

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import Content from './content/content';
 import Search from './main_search/search';
 import Profile from './profile/profile';
 import Results from './results_list/results';
+import Group from './investigation_group/group'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Results />
+    <Search />
   </React.StrictMode>
 );
 
