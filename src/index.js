f66947c999a962d5906bb7508d1f7071b3cf2638
Klariff <jpvera.f@gmail.com>
2023-05-02 17:46:32 -0500

    mapas con leaflet en plantilla

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Main from './main/main';
+import "leaflet/dist/leaflet.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
